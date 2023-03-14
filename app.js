d9786b2c53264e9c0c6ec19afee32832f5344ef1
rr37 <ay870527@gmail.com>
2023-03-14 11:59:25 +0800

    first upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  // create a variable to store movieOne
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/search', (req, res) => {
+  const  keyword = req.query.keyword
+  const movies = movieList.results.filter(movie => {
+    return movie.title.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { movies: movies, keyword: keyword })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movie = movieList.results.find(
+    movie => movie.id.toString() === req.params.movie_id
+  )
+  res.render('show', { movie: movie})
+})
+
+// start and listen on the Express server
+app.listen(port, (req, res) => {
+  console.log(`Express is listening on localhost: ${port}`)
+})
